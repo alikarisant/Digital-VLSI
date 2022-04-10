91b5e5623ba11328250b5192fc268b12911a44a4
alikarisant <82325809+alikarisant@users.noreply.github.com>
2022-04-10 14:02:48 +0300

    Update Sychronous_Systolic_4bit_Multiplier.vhd

--- LAB 3/Sychronous_Systolic_4bit_Multiplier.vhd
+++ LAB 3/Sychronous_Systolic_4bit_Multiplier.vhd
@@ -27,64 +27,47 @@ architecture Structural of Sychronous_Systolic_4bit_Multiplier is
        Bout: out std_logic
         );
   end component;
-  signal B3 : std_logic_vector(9 downto 0);
-  signal Dp0 : std_logic_vector(8 downto 0):= "000000000";
-  signal Dp1, B2 : std_logic_vector(7 downto 0);
-  signal Dp2, A3 : std_logic_vector(6 downto 0);
-  signal Dp3, A2, B1 : std_logic_vector(5 downto 0);
-  signal Dp4 : std_logic_vector(4 downto 0);
-  signal Dp5 : std_logic_vector(3 downto 0);
-  signal C0,C1,C2,A1 :  std_logic_vector(4 downto 0);
-  signal A0,C3, B0 :  std_logic_vector(3 downto 0);  
-begin
-
-    A3(2) <= '0'; Dp1(2) <= '0'; Dp1(3) <= '0'; Dp1(4) <= '0'; Dp1(5) <= '0'; Dp1(6) <= '0'; Dp1(7) <= '0';
-    A3(1) <= '0'; A3(0) <= '0'; A2(1) <= '0'; A2(0) <= '0'; A1(0) <= '0';
-    B1(1) <= '0';
-    B1(0) <= '0';
-    B2(3) <= '0';
-    B2(2) <= '0';
-    B2(1) <= '0';
-    B2(0) <= '0';
-    B3(5) <= '0';
-    B3(4) <= '0';
-    B3(3) <= '0';
-    B3(2) <= '0';
-    B3(1) <= '0';
-    B3(0) <= '0';
-    Dp2(3) <= '0'; Dp2(4) <= '0'; Dp2(5) <= '0'; Dp2(6) <= '0';
-    Dp3(4) <= '0'; Dp3(5) <= '0'; Dp4(4) <= '0'; C0(4) <= '0'; C1(4) <= '0'; C2(4) <= '0'; 
-  pipeline_proc: process(clk, rst)
-      begin
-    if(rst = '1') then
-        Dp0 <= "000000000";
-     elsif(clk'event and clk = '1') then
-        Product(0) <= Dp0(8); Product(1) <= Dp1(7); Product(2) <= Dp2(6); Product(3) <= Dp3(5); Product(4) <= Dp4(4); Product(5) <= Dp5(3);
-        FOR i IN 1 TO 8 LOOP
-            Dp0(i) <= Dp0(i-1);
-        END LOOP;
-        
-        FOR i IN 2 TO 7 LOOP
-            Dp1(i) <= Dp1(i-1);
-        END LOOP;
-        
-        FOR i IN 3 TO 6 LOOP
-            Dp2(i) <= Dp2(i-1);
-        END LOOP;
-        
-        Dp3(5) <= Dp3(4); Dp3(4) <= Dp3(3); Dp4(4) <= Dp4(3);
-        A1(0) <= A(1); A2(1) <= A2(0); A2(0) <= A(2); A3(2) <= A3(1); A3(1) <= A3(0); A3(0) <= A(3);
-        B1(1) <= B1(0); B1(0) <= B(1); B2(3) <= B2(2); B2(2) <= B2(1); B2(1) <= B2(0); B2(0) <= B(2);
-        C0(4) <= C0(3); C1(4) <= C1(3); C2(4) <= C2(3);
-        FOR i IN 1 TO 5 LOOP
-            B3(i) <= B3(i-1);
-        END LOOP;
-        B3(0) <= B(3);
-        
-         
-     end if;
-  end process;
   
+  --Signals regarding subtotals
+  signal P0 : std_logic_vector(9 downto 0):="0000000000"; --ready
+  signal P1 : std_logic_vector(8 downto 0):="000000000"; --ready
+  signal P2 : std_logic_vector(7 downto 0):="00000000"; --ready
+  signal P3 : std_logic_vector(6 downto 0):="0000000"; --ready
+  signal P4 : std_logic_vector(4 downto 0):="00000"; --ready 
+  signal P5 : std_logic_vector(2 downto 0):="000"; --ready
+  signal P6 : std_logic:='0'; --ready
+  signal P7 : std_logic:='0'; --ready
+  
+  --signals regarding internal transports
+  signal C0 : std_logic_vector(3 downto 0):="0000"; --Cout each gate
+  signal C1 : std_logic_vector(3 downto 0):="0000"; --Cout each gate
+  signal C2 : std_logic_vector(3 downto 0):="0000"; --Cout each gate
+  signal C3 : std_logic_vector(3 downto 0):="0000"; --Cout each gate
+  signal A0 : std_logic_vector(3 downto 0):="0000"; --diagonal
+  signal A1 : std_logic_vector(3 downto 0):="0000"; --diagonal
+  signal A2 : std_logic_vector(3 downto 0):="0000"; --diagonal
+  signal A3 : std_logic_vector(3 downto 0):="0000"; --diagonal
+  signal B0 : std_logic_vector(3 downto 0):="0000"; --straight
+  signal B1 : std_logic_vector(3 downto 0):="0000"; --straight
+  signal B2 : std_logic_vector(3 downto 0):="0000"; --straight
+  signal B3 : std_logic_vector(3 downto 0):="0000"; --straight
+  
+  --delays for A
+  signal delayA1 : std_logic:='0';
+  signal delayA2,delayA3 : std_logic:='0';
+  signal delayA4,delayA5,delayA6 : std_logic:='0';
+  
+  --delays for B
+  signal delayB1,delayB2 : std_logic:='0';
+  signal delayB3,delayB4 : std_logic:='0';
+  signal delayB5,delayB6,delayB7 : std_logic:='0';
+  signal delayB8,delayB9,delayB10 : std_logic:='0';
+  signal delayB11,delayB12,delayB13,delayB14 : std_logic:='0';
+  --delays for C
+  signal delayC1,delayC3 : std_logic:='0';
+  signal delayC2,delayC4 : std_logic:='0';
+  signal delayC5,delayC6 : std_logic:='0';
+begin
   
   Building_Block_INSTANCE_0: Building_Block
     port map ( clk => clk,
@@ -93,190 +76,344 @@ begin
                Ain => A(0),
                Bin => B(0),
                Cin => '0',
-               Sout => Dp0(0),
+               Sout => P0(0),
                Cout => C0(0),
                Aout => A0(0),
                Bout => B0(0));
 
-Building_Block_INSTANCE_1: Building_Block
+ delay_P0:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    Product(0) <= P0(9);
+    P0(9) <= P0(8);
+    P0(8) <= P0(7);
+    P0(7) <= P0(6);
+    P0(6) <= P0(5);
+    P0(5) <= P0(4);
+    P0(4) <= P0(3);
+    P0(3) <= P0(2);
+    P0(2) <= P0(1);
+    P0(1) <= P0(0);
+  end if;
+end process;
+               
+ delay_1:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayA1 <= A(1);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_1: Building_Block
     port map ( clk => clk,
                rst => rst,
                Sin => '0',
-               Ain => A1(0),
+               Ain => delayA1,
                Bin => B0(0),
                Cin => C0(0),
-               Sout => Dp1(0),
+               Sout =>P1(0),
                Cout => C0(1),
-               Aout => A1(1),
-               Bout => B0(1));    
+               Aout => A1(0),
+               Bout => B0(1));
+               
+ delay_2:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayA2 <= delayA3;
+    delayA3 <= A(2);
+  end if;
+end process;
 
-Building_Block_INSTANCE_2: Building_Block
+  Building_Block_INSTANCE_2: Building_Block
     port map ( clk => clk,
                rst => rst,
                Sin => '0',
-               Ain => A2(1),
+               Ain => delayA2,
                Bin => B0(1),
                Cin => C0(1),
-               Sout => Dp2(0),
+               Sout =>P2(0),
                Cout => C0(2),
-               Aout => A2(2),
+               Aout => A2(0),
                Bout => B0(2));
 
-Building_Block_INSTANCE_3: Building_Block
+ delay_3:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayA4 <= delayA5;
+    delayA5 <= delayA6;
+    delayA6 <= A(3);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_3: Building_Block
     port map ( clk => clk,
                rst => rst,
                Sin => '0',
-               Ain => A3(2),
+               Ain => delayA4,
                Bin => B0(2),
-               Cin => C0(2),
-               Sout => Dp3(0),
+               Cin => C0(1),
+               Sout =>P3(0),
                Cout => C0(3),
-               Aout => A3(3),
-               Bout => B0(3));    
+               Aout => A3(0),
+               Bout => B0(3));
 
-Building_Block_INSTANCE_4: Building_Block
+ delay_4:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayB1 <= delayB2;
+    delayB2 <= B(1);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_4: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => Dp1(0),
+               Sin => P1(0),
                Ain => A0(0),
-               Bin => B1(1),
+               Bin => delayB1,
                Cin => '0',
-               Sout => Dp1(1),
+               Sout =>P1(1),
                Cout => C1(0),
                Aout => A0(1),
-               Bout => B1(2));
-               
-Building_Block_INSTANCE_5: Building_Block
+               Bout => B1(0));
+
+ delay_P1:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    Product(1) <= P1(8);
+    P1(8) <= P1(7);
+    P1(7) <= P1(6);
+    P1(6) <= P1(5);
+    P1(5) <= P1(4);
+    P1(4) <= P1(3);
+    P1(3) <= P1(2);
+    P1(2) <= P1(1);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_5: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => Dp2(0),
-               Ain => A1(1),
-               Bin => B1(2),
+               Sin => P2(0),
+               Ain => A1(0),
+               Bin => B1(0),
                Cin => C1(0),
-               Sout => Dp2(1),
+               Sout =>P2(1),
                Cout => C1(1),
-               Aout => A1(2),
+               Aout => A1(1),
+               Bout => B1(1));
+
+  Building_Block_INSTANCE_6: Building_Block
+    port map ( clk => clk,
+               rst => rst,
+               Sin => P3(0),
+               Ain => A2(0),
+               Bin => B1(1),
+               Cin => C1(1),
+               Sout =>P3(1),
+               Cout => C1(2),
+               Aout => A2(1),
+               Bout => B1(2));
+
+ delay_5:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayC1 <= delayC2;
+    delayC2 <= C0(3);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_7: Building_Block
+    port map ( clk => clk,
+               rst => rst,
+               Sin => delayC1,
+               Ain => A3(0),
+               Bin => B1(2),
+               Cin => C1(2),
+               Sout =>P4(0),
+               Cout => C1(3),
+               Aout => A3(1),
                Bout => B1(3));
 
-Building_Block_INSTANCE_6: Building_Block
-    port map ( clk => clk,
-               rst => rst,
-               Sin => Dp3(0),
-               Ain => A2(2),
-               Bin => B1(3),
-               Cin => C1(1),
-               Sout => Dp3(1),
-               Cout => C1(2),
-               Aout => A2(3),
-               Bout => B1(4));    
+ delay_6:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayB3 <= delayB4;
+    delayB5 <= delayB6;
+    delayB6 <= delayB7;
+    delayB7 <= B(2);
+  end if;
+end process;
 
-Building_Block_INSTANCE_7: Building_Block
+  Building_Block_INSTANCE_8: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => C0(4),
-               Ain => A3(3),
-               Bin => B1(4),
-               Cin => C1(2),
-               Sout => Dp4(1),
-               Cout => C1(3),
-               Aout => A3(4),
-               Bout => B1(5));
-               
-Building_Block_INSTANCE_8: Building_Block
-    port map ( clk => clk,
-               rst => rst,
-               Sin => Dp2(1),
+               Sin => P2(1),
                Ain => A0(1),
-               Bin => B2(3),
+               Bin => delayB3,
                Cin => '0',
-               Sout => Dp2(2),
+               Sout =>P2(2),
                Cout => C2(0),
                Aout => A0(2),
-               Bout => B2(4));
-               
-Building_Block_INSTANCE_9: Building_Block
+               Bout => B2(0));
+
+ delay_P2:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    Product(2) <= P2(7);
+    P2(7) <= P2(6);
+    P2(6) <= P2(5);
+    P2(5) <= P2(4);
+    P2(4) <= P2(3);
+    P2(3) <= P2(2);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_9: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => Dp3(1),
-               Ain => A1(2),
-               Bin => B2(4),
+               Sin => P3(1),
+               Ain => A1(1),
+               Bin => B2(0),
                Cin => C2(0),
-               Sout => Dp3(2),
+               Sout =>P3(2),
                Cout => C2(1),
-               Aout => A1(3),
-               Bout => B2(5));
+               Aout => A1(2),
+               Bout => B2(1));
 
-Building_Block_INSTANCE_10: Building_Block
+  Building_Block_INSTANCE_10: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => Dp4(1),
-               Ain => A2(3),
-               Bin => B2(5),
+               Sin => P4(0),
+               Ain => A2(1),
+               Bin => B2(1),
                Cin => C2(1),
-               Sout => Dp4(2),
+               Sout =>P4(1),
                Cout => C2(2),
-               Aout => A2(4),
-               Bout => B2(6));    
+               Aout => A2(2),
+               Bout => B2(2));
 
-Building_Block_INSTANCE_11: Building_Block
+ delay_7:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayC3 <= delayC4;
+    delayC4 <= C1(3);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_11: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => C1(4),
-               Ain => A3(4),
-               Bin => B2(6),
+               Sin => delayC3,
+               Ain => A3(1),
+               Bin => B2(2),
                Cin => C2(2),
-               Sout => Dp5(2),
+               Sout =>P5(0),
                Cout => C2(3),
-               Aout => A3(5),
-               Bout => B2(7));
+               Aout => A3(2),
+               Bout => B2(3));
 
-Building_Block_INSTANCE_12: Building_Block
+ delay_8:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayB8 <= delayB9;
+    delayB9 <= delayB10;
+    delayB10 <= delayB11;
+    delayB12 <= delayB13;
+    delayB13 <= delayB14;
+    delayB14 <= B(3);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_12: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => Dp3(2),
+               Sin => P3(2),
                Ain => A0(2),
-               Bin => B3(5),
+               Bin => delayB8,
                Cin => '0',
-               Sout => Dp3(3),
+               Sout =>P3(3),
                Cout => C3(0),
                Aout => A0(3),
-               Bout => B3(6));
+               Bout => B3(0));
+ 
+ delay_P3:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    Product(3) <= P3(6);
+    P3(6) <= P3(5);
+    P3(5) <= P3(4);
+    P3(4) <= P3(3);    
+  end if;
+end process; 
                
-Building_Block_INSTANCE_13: Building_Block
+  Building_Block_INSTANCE_13: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => Dp4(2),
-               Ain => A1(3),
-               Bin => B3(6),
+               Sin => P4(1),
+               Ain => A1(2),
+               Bin => B3(0),
                Cin => C3(0),
-               Sout => Dp4(3),
+               Sout =>P4(2),
                Cout => C3(1),
-               Aout => A1(4),
-               Bout => B3(7));
+               Aout => A1(3),
+               Bout => B3(1));
 
-Building_Block_INSTANCE_14: Building_Block
+ delay_P4:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    Product(4) <= P4(4);
+    P4(4) <= P4(3);
+    P4(3) <= P4(2);   
+  end if;
+end process;
+
+  Building_Block_INSTANCE_14: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => Dp5(2),
-               Ain => A2(4),
-               Bin => B3(7),
+               Sin => P5(0),
+               Ain => A2(2),
+               Bin => B3(1),
                Cin => C3(1),
-               Sout => Dp5(3),
+               Sout =>P5(1),
                Cout => C3(2),
-               Aout => A2(5),
-               Bout => B3(8));    
+               Aout => A2(3),
+               Bout => B3(2));
 
-Building_Block_INSTANCE_15: Building_Block
+ delay_P5:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    Product(5) <= P5(2);
+    P5(2) <= P5(1);   
+  end if;
+end process;
+
+ delay_9:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    delayC5 <= delayC6;
+    delayC6 <= C2(3);
+  end if;
+end process;
+
+  Building_Block_INSTANCE_15: Building_Block
     port map ( clk => clk,
                rst => rst,
-               Sin => C2(4),
-               Ain => A3(5),
-               Bin => B3(8),
+               Sin => delayC5,
+               Ain => A3(2),
+               Bin => B3(2),
                Cin => C3(2),
-               Sout => Product(6), 
-               Cout => Product(7),
-               Aout => A3(6),
-               Bout => B3(9));
+               Sout =>P6,
+               Cout => P7,
+               Aout => A3(3),
+               Bout => B3(3));
 
-
+ delay_P6_P7:process (clk)		--5 clock cycles
+begin
+  if rising_edge(clk) then
+    Product(7) <= P7;
+    Product(6) <= P6;   
+  end if;
+end process;
+           
 end Structural;
